d573a38259c4b6f84959d6fbce681388568f8a17
santyago43 <santiagolopezvallejo@gmail.com>
2024-07-28 14:46:58 -0500

    Admin Page done

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Tutorial de Java',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Curso de HTML',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Webinar de C#',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
